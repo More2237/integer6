bb442aeb1ab75c2fe2a244bbf95d983758bb259a
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 10:52:22 +0500

    nbv

---
+++ integer6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace integer6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double taqsim = a / b;
+            double vzyatya = a % b;
+            Console.WriteLine(taqsim);
+            Console.WriteLine(vzyatya);
+            
+            
+        }
+    }
+}
